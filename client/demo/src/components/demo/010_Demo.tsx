c1df68d98cdedc958d8ed9cde661d2d5a2bfbb57
Yury <rip95_95@mail.ru>
2024-07-26 19:20:22 +0300

    Move ReactToastify.css to App.css

--- client/demo/src/components/demo/010_Demo.tsx
+++ client/demo/src/components/demo/010_Demo.tsx
@@ -4,7 +4,6 @@ import { Dialogs } from "./900_Dialogs";
 import { ModelSlotControl } from "./b00_ModelSlotControl";
 import { Dialogs2 } from "./910_Dialogs2";
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 export const Demo = () => {
     return (
