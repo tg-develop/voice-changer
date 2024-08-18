543ff0b530e58185e4388e0ab992a4150e554eaa
Yury <rip95_95@mail.ru>
2024-08-18 13:18:49 +0300

    Reorder pytorch and onnxruntime imports

--- server/voice_changer/RVC/pitchExtractor/RMVPEOnnxPitchExtractor.py
+++ server/voice_changer/RVC/pitchExtractor/RMVPEOnnxPitchExtractor.py
@@ -1,11 +1,11 @@
 import numpy as np
+import torch
+import onnxruntime
 from const import PitchExtractorType
 from voice_changer.common.OnnxLoader import load_onnx_model
 from voice_changer.RVC.pitchExtractor.PitchExtractor import PitchExtractor
 from voice_changer.common.deviceManager.DeviceManager import DeviceManager
 from voice_changer.common.MelExtractor import MelSpectrogram
-import onnxruntime
-import torch
 
 class RMVPEOnnxPitchExtractor(PitchExtractor):
 
